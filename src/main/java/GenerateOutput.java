617b48eab6e690f357d555f1e3d793aa9a947dc1
Bobby Singla <bobbysingla1112@gmail.com>
2019-04-10 12:29:46 +0530

    second commit

---
+++ src/main/java/GenerateOutput.java
@@ -0,0 +1,28 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.List;
+
+public class GenerateOutput {
+    private final Path outputFilepath = Paths.get("src/main/resources/", "OutputFile.csv");
+    public void generateFile(List<Student> student) throws IOException {
+        OutputStream os = new FileOutputStream(outputFilepath.toFile());
+        PrintWriter fileWriter = new PrintWriter(os);
+//        Iterator iterator = student.iterator();
+//        while (iterator.hasNext()) {
+//            fileWriter.append((Character) iterator.next());
+//        }
+        for (Student rowData : student) {
+            fileWriter.write(rowData.getName()+"," +rowData.getBatch()+","+rowData.getDorm()+","+rowData.getRoom()
+            +","+rowData.getGpa()+"\n");
+//            fileWriter.append(String.join(",", rowData));
+//            fileWriter.append("\n");
+        }
+        fileWriter.flush();
+        fileWriter.close();
+    }
+}
